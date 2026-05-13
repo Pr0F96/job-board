e43fb627d08f2baaeb70ef5cb01cdbed24d4335e
Pr0F96 <140273194+Pr0F96@users.noreply.github.com>
2026-05-13 17:16:44 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata = {
   description: 'Find your next opportunity',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
